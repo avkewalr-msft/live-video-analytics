bfd471c13d3fdbff9c831e836273f15b5174f0fe
Nicolas Botto <nicolas@agilesight.com>
2020-10-06 22:03:48 +0000

    Add missing install argument (#120)

--- utilities/video-analysis/deepstream/app/start.sh
+++ utilities/video-analysis/deepstream/app/start.sh
@@ -6,5 +6,5 @@ cp /opt/nvidia/deepstream/deepstream-5.0/lib/pyds.so /app
 cp /opt/nvidia/deepstream/deepstream-5.0/lib/setup.py /app
 # Copy nvidia deepstream models
 cp -r /opt/nvidia/deepstream/deepstream-5.0/samples/models/ /app
-python3 setup.py
+python3 setup.py install
 python3 main.py -p 5001
